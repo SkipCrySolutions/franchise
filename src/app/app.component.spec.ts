fd61da6b8b3b0587080c1961b6ef88afe31b6231
SkipCrySolutions <162356356+SkipCrySolutions@users.noreply.github.com>
2024-10-27 22:26:07 +0530

    second commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'admin' title`, () => {
+  it(`should have the 'franchise' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('admin');
+    expect(app.title).toEqual('franchise');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, admin');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, franchise');
   });
 });
